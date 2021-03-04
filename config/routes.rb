db5ea8a82d3ff3264c3ac30e63d68e2706a358c6
Mark Ghaida <markghaida@gmail.com>
2021-03-04 02:17:37 -0500

    properly persisting post request for bookmark...still need to render to frontend without referesh

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :bookmark_tags
   # resources :bookmarks
   get '/bookmarks', to: 'bookmarks#index'
+  get '/bookmarks/:id', to: 'bookmarks#show'
   post '/bookmarks', to: 'bookmarks#create'
+  get '/bookmarks/:id', to: 'bookmarks#destroy'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
