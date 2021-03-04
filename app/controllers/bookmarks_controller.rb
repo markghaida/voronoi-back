50ba8b160075efde4fc22d8ed9482066202e6be6
Mark Ghaida <markghaida@gmail.com>
2021-03-04 00:24:58 -0500

    created custom routes for get and post requests in routes.rb to test webscraping fetch

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,15 +9,17 @@ class BookmarksController < ApplicationController
         render json: bookmark 
     end 
 
-    def create 
-        # add code to receive the url, then do the follwoing web scraping 
-        page = MetaInspector.new(params[:url])
-        bookmark = Bookmark.create(url: page.url, image: page.images.favicon, h1: page.h1, body: page.description, user_id: 1)
-        puts bookmark
-        # bookmark = Bookmark.create(url: params[:url], image: params[:image], h1: params[:h1], body: params[:body], user_id: params[:user_id])
-        render json: bookmark 
-    end 
-    
+    # def create 
+    #     # add code to receive the url, then do the follwoing web scraping 
+        
+    #     page = MetaInspector.new(params[:url])
+    #     # puts "hello from bookmarks controller", page
+    #     bookmark = Bookmark.create(url: page.url, image: page.images.favicon, h1: page, body: page.description, user_id: 1)
+    #     # puts bookmark
+    #     # bookmark = Bookmark.create(url: params[:url], image: params[:image], h1: params[:h1], body: params[:body], user_id: params[:user_id])
+    #     render json: bookmark 
+    # end 
+
 
     def update 
         bookmark = Bookmark.update(url: params[:url], image: params[:image], h1: params[:h1], body: params[:body], user_id: params[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :tags
   resources :bookmark_tags
-  resources :bookmarks
+  # resources :bookmarks
+  get '/bookmarks', to: 'bookmarks#index'
+  post '/bookmarks', to: 'bookmarks#create'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
