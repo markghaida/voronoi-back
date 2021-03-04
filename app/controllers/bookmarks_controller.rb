7c7223fb0bab94278272100ab579aa96e30e8a6e
Mark Ghaida <markghaida@gmail.com>
2021-03-04 00:30:15 -0500

    changed the create action controller to test if its getting hit and persists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,16 +9,16 @@ class BookmarksController < ApplicationController
         render json: bookmark 
     end 
 
-    # def create 
+    def create 
     #     # add code to receive the url, then do the follwoing web scraping 
         
-    #     page = MetaInspector.new(params[:url])
-    #     # puts "hello from bookmarks controller", page
-    #     bookmark = Bookmark.create(url: page.url, image: page.images.favicon, h1: page, body: page.description, user_id: 1)
+        page = MetaInspector.new(params[:url])
+        # puts "hello from bookmarks controller", page
+        bookmark = Bookmark.create(url: page.url, image: page.images.favicon, h1: page, body: page.description, user_id: 1)
     #     # puts bookmark
     #     # bookmark = Bookmark.create(url: params[:url], image: params[:image], h1: params[:h1], body: params[:body], user_id: params[:user_id])
-    #     render json: bookmark 
-    # end 
+        render json: bookmark 
+    end 
 
 
     def update 
